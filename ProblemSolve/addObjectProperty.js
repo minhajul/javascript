1563c9cfebdd1fe5e47dd5edc0b41ce6e9ea2ec8
minhajul <minhajul@maya.com.bd>
2017-02-11 12:53:45 +0600

    Solved addObjectProperty problem

---
+++ ProblemSolve/addObjectProperty.js
@@ -0,0 +1,30 @@
+// Write a function called "addObjectProperty".
+//
+// Given two objects and a key, "addObjectProperty" sets a new property on the 1st object at the given key. The value of that new property is the entire 2nd object.
+//
+// var person1 = {
+//   name: 'Joe Blow',
+//   role: 'schlub'
+// };
+// var person2 = {
+//   name: 'Mr. Burns',
+//   role: 'supervisor'
+// };
+// addObjectProperty(person1, 'manager', person2);
+// console.log(person1.manager); // --> { name: 'Mr. Burns', role: 'supervisor' }
+
+function addObjectProperty(obj1, key, obj2) {
+   obj1[key] = obj2;
+   return obj1;
+}
+
+var person1 = {
+  name: 'Joe Blow',
+  role: 'schlub'
+};
+var person2 = {
+  name: 'Mr. Burns',
+  role: 'supervisor'
+};
+addObjectProperty(person1, 'manager', person2);
+console.log(person1.manager); //{ name: 'Mr. Burns', role: 'supervisor' }
