ae51f1ae4b62817de73875b4fe4c605dd7a64844
minhajul <minhajul@maya.com.bd>
2017-02-11 12:25:19 +0600

    Solved extend problem

---
+++ ProblemSolve/extend.js
@@ -0,0 +1,37 @@
+// Given two objects, "extend" adds properties from the 2nd object to the 1st object.
+//
+// Notes:
+// * Add any keys that are not in the 1st object.
+// * If the 1st object already has a given key, ignore it (do not overwrite the property value).
+// * Do not modify the 2nd object at all.
+//
+// var obj1 = {
+//   a: 1,
+//   b: 2
+// };
+// var obj2 = {
+//   b: 4,
+//   c: 3
+// };
+//
+// extend(obj1, obj2);
+//
+// console.log(obj1); // --> {a: 1, b: 2, c: 3}
+// console.log(obj2); // --> {b: 4, c: 3}
+
+
+function extend(obj1, obj2) {
+  let keys = Object.keys(obj2);
+	var results = {};
+	for(var key in obj2){
+    if(!obj1.hasOwnProperty(key)){
+    	obj1[key] = obj2[key]
+    }
+  }
+	return obj1;
+}
+
+var obj1 = {a: 1,b: 2};
+var obj2 = {b: 4,c: 3};
+
+extend(obj1, obj2);
