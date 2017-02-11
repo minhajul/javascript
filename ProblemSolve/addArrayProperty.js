23ca6cb91c8d57832055a283728a7598882db6be
minhajul <minhajul@maya.com.bd>
2017-02-11 12:40:15 +0600

    Solved addArrayProperty problem

---
+++ ProblemSolve/addArrayProperty.js
@@ -0,0 +1,19 @@
+// Write a function called "addArrayProperty".
+//
+// Given an object, a key, and an array, "addArrayProperty" sets a new property on the object at the given key, with its value set to the given array.
+//
+// var myObj = {};
+// var myArray = [1, 3];
+// addArrayProperty(myObj, 'myProperty', myArray);
+// console.log(myObj.myProperty); // --> [1, 3]
+
+
+function addArrayProperty(obj, key, arr) {
+   obj[key] = arr;
+   return obj;
+}
+
+var myObj = {};
+var myArray = [1, 3];
+addArrayProperty(myObj, 'myProperty', myArray);
+console.log(myObj.myProperty);
