95ab8270d6c174ab71d721c1969a2364b53ffbdd
minhajul <minhajul@maya.com.bd>
2017-02-11 13:02:58 +0600

    Solved addProperty problem

---
+++ ProblemSolve/addProperty.js
@@ -0,0 +1,16 @@
+// Write a function called "addProperty".
+//
+// Given an object, and a key, "addProperty" sets a new property on the given object with a value of true.
+//
+// var myObj = {};
+// addProperty(myObj, 'myProperty');
+// console.log(myObj.myProperty); // --> true
+
+function addProperty(obj, key) {
+   obj[key] = true;
+   return obj;
+}
+
+var myObj = {};
+addProperty(myObj, 'myProperty');
+console.log(myObj.myProperty); // --> true
