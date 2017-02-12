d0e1b58a674d4d8e6631d5f6577bd3c179c88641
minhajul <minhajul@maya.com.bd>
2017-02-12 22:06:29 +0600

    Solved fizzBuzz problem using javascript

---
+++ ProblemSolve/fizzBuzz.js
@@ -0,0 +1,23 @@
+function fizzbuzz(n){
+	let results = [];
+
+	for(let i = 1; i <= n; i++){
+		let add = '';
+
+		if(i % 3 === 0){
+			add += 'Fizz';
+		}
+
+		if(i % 5 === 0){
+			add += 'Buzz';
+		}
+
+		if(add === ''){
+			results.push(i);
+		}else{
+			results.push(add);
+		}
+	}
+
+	return results;
+}
