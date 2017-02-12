5ebeb64056ce026c314f2c3b8a27de8f7d53cc37
minhajul <minhajul@maya.com.bd>
2017-02-12 22:05:35 +0600

    Solved twoSum problem using javascript

---
+++ ProblemSolve/twoSum.js
@@ -0,0 +1,18 @@
+function twoSum(array, S){
+
+	let hashTable = {};
+
+	for(let i = 0; i <= array.length ; i++){
+		sumMinusElement = S - array[i];
+
+		if(sumMinusElement !== undefined){
+			return true;
+		}
+
+		hashTable[array[i]] = true;
+	}
+
+	return false
+}
+
+twoSum([3, 2, 3, 4], 2)
