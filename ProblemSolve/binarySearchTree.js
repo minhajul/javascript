a2f5b3dedd0fa719d8af463bc6a0b1b65a7f0983
minhajul <minhajul@maya.com.bd>
2017-02-17 00:44:54 +0600

    Practice binary search tree

--- ProblemSolve/binarySearchTree.js
+++ ProblemSolve/binarySearchTree.js
@@ -1,34 +1,59 @@
-
-
-class Node{
+class Node {
     constructor(value){
-        this.value = value;
+        this.value= value;
         this.left = undefined;
         this.right = undefined;
     }
 }
 
-
-class binarySearchTree{
-    constructor(value){
-        this.root = new Node(value);
+class BinarySearchTree{
+    constructor(root){
+        this.root = root;
     }
 
     add(newNode){
-        let root = this.root;
-        if(newNode.value < root.value){
-            root.left = newNode;
-        } else{
-            root.right = newNode;
+        let currentNode = this.root;
+        while(currentNode){
+            if(newNode.value < currentNode.value){
+                if(currentNode.left === undefined){
+                    currentNode.left = newNode;
+                    break;
+                } else{
+                    currentNode = currentNode.left;
+                }
+            } else{
+                if(currentNode.right === undefined){
+                    currentNode.right = newNode;
+                    break;
+                } else{
+                    currentNode = currentNode.right;
+                }
+            }
         }
     }
 }
 
 
+
 let root = new Node(40);
-let treeTeam = new binarySearchTree(root);
-treeTeam.add(new Node(25));
-treeTeam.add(new Node(78));
-treeTeam.add(new Node(79));
+let treeTeam = new BinarySearchTree(root);
+
+let first = new Node(25);
+treeTeam.add(first);
+
+let second = new Node(78);
+treeTeam.add(second);
+
+let third = new Node(10);
+treeTeam.add(third);
+
+let fourth = new Node(65);
+treeTeam.add(fourth);
+
+let fifth = new Node(75);
+treeTeam.add(fifth);
+
+let sixth = new Node(30);
+treeTeam.add(sixth);
 
 console.log(treeTeam);
